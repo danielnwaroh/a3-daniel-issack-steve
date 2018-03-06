40df012e663b4079a7d2c2f341ab0704e8c11ac6
danielnwaroh <daniel.nwaroh@gmail.com>
2018-03-06 15:50:57 -0700

    More stuff

--- a3/source/main.s
+++ a3/source/main.s
@@ -1,15 +1,97 @@
 @ CPSC 359 L01 Assignment 3
+@ Daniel Nwaroh & Issack John & Steve Khanna
 
 @ Code section
 .section    .text
 
 .global main
 
-main: 	
+main:
 		ldr		r0,	=names
 		bl		printf
-		
+		b		request
+
 stop:	b		stop
+
+request:
+		ldr		r0,	=pressButton
+		bl		printf
+
+
+
+
+
+printB:
+		ldr		r0,	=butB
+		bl		printf
+
+		b			request
+
+printY:
+		ldr		r0,	=butY
+		bl		printf
+
+		b		request
+
+printSelect:
+		ldr		r0,	=butSelect
+		bl		printf
+
+		b		request
+
+printUp:
+		ldr		r0,	=butUp
+		bl		printf
+
+		b		request
+		
+printDown:
+		ldr		r0,	=butDown
+		bl		printf
+
+		b		request
+		
+printLeft:
+		ldr		r0,	=butLeft
+		bl		printf
+
+		b		request
+		
+printRight:
+		ldr		r0,	=butRight
+		bl		printf
+
+		b		request
+
+printA:
+		ldr		r0,	=butA
+		bl		printf
+
+		b		request
+		
+printX:
+		ldr		r0,	=butX
+		bl		printf
+
+		b		request
+		
+printLb:
+		ldr		r0,	=butLb
+		bl		printf
+
+		b		request
+		
+printRb:
+		ldr		r0,	=butRb
+		bl		printf
+
+		b		request
+		
+printEnd:
+		ldr		r0,	=end
+		bl		printf
+		
+		b		stop
 
 @ Data section
 .section    .data
@@ -29,7 +111,6 @@ butY:
 .asciz	"You have pressed Y\n"
 butSelect:
 .asciz	"You have pressed select\n"
-butStart:
 butUp:
 .asciz	"You have pressed Joy-pad UP\n"
 butDown:
